14706de7947db2e236308d0e01083f63d19fa227
Edvinas <edvinasbureika@gmail.com>
2026-02-22 22:01:18 +0200

    Data types

--- 03-data-types/index.php
+++ 03-data-types/index.php
@@ -12,3 +12,47 @@
 - NULL
 - Resource
 */
+
+// Those 2 are different things
+$string_single_qoutes = 'Hello, World!';
+$string = "Hello, World!";
+
+var_dump($string);
+echo '<br/>';
+echo getType($string);
+
+$integer = 42;
+
+var_dump($integer);
+echo '<br/>';
+
+$float = 3.14;
+
+var_dump($float);
+echo '<br/>';
+
+$boolean = true;
+
+var_dump($boolean);
+echo '<br/>';
+
+$array = [1, 2, 3, 4, 5];
+
+var_dump($array);
+echo '<br/>';
+
+$null = null;
+
+var_dump($null);
+echo '<br/>';
+
+// Object
+$person = new stdClass();
+var_dump($person);
+echo '<br/>';
+
+// Resource
+$file = fopen('sample.txt', 'r');
+
+var_dump($file);
+echo '<br/>';
