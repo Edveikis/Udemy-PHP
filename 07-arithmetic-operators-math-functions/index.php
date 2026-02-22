c23218bb4c7f07ccf76a619f877fe2447a3b5cec
Edvinas <edvinasbureika@gmail.com>
2026-02-22 22:59:01 +0200

    Math

--- 07-arithmetic-operators-math-functions/index.php
+++ 07-arithmetic-operators-math-functions/index.php
@@ -9,6 +9,57 @@
 | `/`      | Division       |
 | `%`      | Modulus        |
 */
+
+$output = null;
+
+$num1 = 20;
+$num2 = 10;
+
+$output = "$num1 + $num2 = " . $num1 + $num2;
+$output = "$num1 + $num2 = " . $num1 - $num2;
+$output = "$num1 + $num2 = " . $num1 * $num2;
+$output = "$num1 + $num2 = " . $num1 / $num2;
+$output = "$num1 + $num2 = " . $num1 % $num2;
+
+// Assignment operator
+$num3 = 10;
+// $num3 = $num3 + 20;
+$num3 += 20;
+
+// Built in functions
+// rand()
+
+$output = rand();
+
+$output = rand(1, 10);
+
+// round()
+$output = round(4.6);
+
+// ceil()
+$output = ceil(4.2);
+
+// floor()
+$output = floor(4.8);
+
+// sqrt()
+$output = sqrt(16);
+
+// pi()
+$output = pi();
+
+// abs()
+$output = abs(-5);
+
+// max()
+$output = max(1,3,3,5,500);
+
+// min()
+$output = min(1,3,3,5,500);
+
+// number_format()
+$output = number_format(1234567.121234, 2, '.', ',');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,6 +80,7 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
+       <p class="text-xl"><?= $output ?></p>
     </div>
   </div>
 </body>
