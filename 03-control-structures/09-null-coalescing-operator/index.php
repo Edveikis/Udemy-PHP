678b05c6adafe4b14565face8fca4e92c4765854
Edvinas <edvinasbureika@gmail.com>
2026-02-26 00:46:40 +0200

    null coalescing operatoir

---
+++ 03-control-structures/09-null-coalescing-operator/index.php
@@ -0,0 +1,8 @@
+<?php
+
+// $favColor = 'red';
+
+// $color = isset($favColor) ? $favColor : 'blue';
+
+// if whatever is on left is null, then use the value on the right
+$color = $favColor ?? 'blue';
