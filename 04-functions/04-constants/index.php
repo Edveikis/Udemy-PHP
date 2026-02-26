ea6067a4acd76b1ab3ee684699851770da46d2d2
Edvinas <edvinasbureika@gmail.com>
2026-02-26 02:11:31 +0200

    constants

---
+++ 04-functions/04-constants/index.php
@@ -0,0 +1,11 @@
+<?php
+
+define('APP_NAME', 'My Awesome App');
+define('APP_VERSION', '1.0.0');
+
+echo APP_NAME;
+echo APP_VERSION;
+
+const DB_NAME = 'my_database';
+const DB_USER   = 'root';
+const DB_PASS   = 'password';       
