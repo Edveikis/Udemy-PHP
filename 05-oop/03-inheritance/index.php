4b25a9a909282114e891f744b56f20619f8fb183
Edvinas <edvinasbureika@gmail.com>
2026-02-27 23:33:48 +0200

    Inheritance

--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,32 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+class Admin extends User
+{
+  public $level; // permission value 1-5
+
+  public function __construct($name, $email, $level)
+  {
+    parent::__construct($name, $email);
+    $this->level = $level;
+  }
+
+  public function getStatus()
+  {
+    return $this->status;
+  } 
+
+  public function login()
+  {
+    echo '[ADMIN] ' . $this->name . ' logged in <br>';
+  }
+}
+
+$admin1 = new Admin('John Doe', 'john@gmail.com', 5);
+
+$admin1->login();  
+
+echo $admin1->getStatus();
+
+// var_dump($admin1);
