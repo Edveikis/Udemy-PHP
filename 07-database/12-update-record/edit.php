9cc5ba4ec46fe6e9157444f0b24ad9ac86c8183f
Edvinas <edvinasbureika@gmail.com>
2026-03-11 03:01:58 +0200

    add check if its a put request

--- 07-database/12-update-record/edit.php
+++ 07-database/12-update-record/edit.php
@@ -8,7 +8,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
   $stmnt = $pdo->prepare('SELECT * FROM posts WHERE id = :id');
   $stmnt->execute(['id' => $id]);
   $post = $stmnt->fetch();
-} else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit']) && $_POST['_method'] === 'put') {
   $stmnt = $pdo->prepare('UPDATE posts SET title = :title, body = :body WHERE id = :id');
   $stmnt->execute([
     'title' => $_POST['title'] ?? '',
@@ -44,7 +44,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
       <h1 class="text-2xl font-semibold mb-6">Update Blog Post</h1>
       <form method="post">
-        <input type="text" name="id" value="<?= $post['id'] ?>" hidden>
+        <input type="hidden" name="_method" value="put">
+        <input type="hidden" name="id" value="<?= $post['id'] ?>">
         <div class="mb-4">
           <label for="title" class="block text-gray-700 font-medium">Title</label>
           <input type="text" id="title" name="title" placeholder="Enter post title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none" value="<?= $post['title'] ?>">
