83aae197df92672801344a68ea844d05f701abdd
Edvinas <edvinasbureika@gmail.com>
2026-03-11 02:59:53 +0200

    update post

--- 07-database/12-update-record/edit.php
+++ 07-database/12-update-record/edit.php
@@ -1,3 +1,29 @@
+<?php
+
+require 'database.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
+  $id = $_GET['id'] ?? null;
+  
+  $stmnt = $pdo->prepare('SELECT * FROM posts WHERE id = :id');
+  $stmnt->execute(['id' => $id]);
+  $post = $stmnt->fetch();
+} else if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+  $stmnt = $pdo->prepare('UPDATE posts SET title = :title, body = :body WHERE id = :id');
+  $stmnt->execute([
+    'title' => $_POST['title'] ?? '',
+    'body' => $_POST['body'] ?? '',
+    'id' => $_POST['id'] ?? null
+  ]);
+  // echo '<pre>';
+  // var_dump($_POST);
+  // echo '</pre>';
+  header('Location: post.php?id=' . $_POST['id']);
+  exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,13 +44,14 @@
     <div class="bg-white p-8 rounded shadow-md w-full max-w-md">
       <h1 class="text-2xl font-semibold mb-6">Update Blog Post</h1>
       <form method="post">
+        <input type="text" name="id" value="<?= $post['id'] ?>" hidden>
         <div class="mb-4">
           <label for="title" class="block text-gray-700 font-medium">Title</label>
-          <input type="text" id="title" name="title" placeholder="Enter post title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none">
+          <input type="text" id="title" name="title" placeholder="Enter post title" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none" value="<?= $post['title'] ?>">
         </div>
         <div class="mb-6">
           <label for="body" class="block text-gray-700 font-medium">Body</label>
-          <textarea id="body" name="body" placeholder="Enter post body" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"></textarea>
+          <textarea id="body" name="body" placeholder="Enter post body" class="w-full px-4 py-2 border rounded focus:ring focus:ring-blue-300 focus:outline-none"><?= $post['body'] ?></textarea>
         </div>
         <div class="flex items-center justify-between">
           <button type="submit" name="submit" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none">
@@ -33,7 +60,6 @@
           <a href="index.php" class="text-blue-500 hover:underline">Back to Posts</a>
         </div>
       </form>
-
     </div>
   </div>
 </body>
